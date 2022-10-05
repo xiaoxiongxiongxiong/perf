b17b3f6e79284d11b3a71a1892f570a3fdbed66e
小熊熊熊 <981303787@qq.com>
2022-10-05 21:35:59 +0800

    完善内存获取功能

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -6,13 +6,13 @@
 #define OS_MEM_BUFF_MAX_LEN 128
 #define OS_MEM_MEMINFO_PATH "/proc/meminfo"
 
-int os_mem_get_sys_info(os_mem_sys_info_t* info)
+int os_mem_get_sys_info(os_mem_sys_info_t * info)
 {
     int ret = 0;
-    FILE* fp = NULL;
+    FILE * fp = NULL;
     uint64_t value = 0;
-    char key[OS_MEM_BUFF_MAX_LEN] = { 0 };
-    char buff[OS_MEM_BUFF_MAX_LEN] = { 0 };
+    char key[OS_MEM_BUFF_MAX_LEN] = {};
+    char buff[OS_MEM_BUFF_MAX_LEN] = {};
 
     if (NULL == info) {
         ret = OS_PERF_ERR_NULLPTR;
@@ -25,12 +25,7 @@ int os_mem_get_sys_info(os_mem_sys_info_t* info)
         goto end;
     }
 
-    while (!feof(fp)) {
-        memset(buff, 0, OS_MEM_BUFF_MAX_LEN);
-        fgets(buff, OS_MEM_BUFF_MAX_LEN, fp);
-
-        value = 0;
-        memset(key, 0, OS_MEM_BUFF_MAX_LEN);
+    while (!feof(fp) && fgets(buff, OS_MEM_BUFF_MAX_LEN, fp)) {
         if (2 != sscanf(buff, "%s %Lu", key, &value))
             continue;
 
@@ -46,98 +41,19 @@ int os_mem_get_sys_info(os_mem_sys_info_t* info)
             info->cached_kb = value;
         else if (0 == strcmp("SwapCached:", key))
             info->swap_cached_kb = value;
-        else if (0 == strcmp("Active:", key))
-            info->active_kb = value;
-        else if (0 == strcmp("Inactive:", key))
-            info->inactive_kb = value;
-        else if (0 == strcmp("Active(anon):", key))
-            info->anon_active_kb = value;
-        else if (0 == strcmp("Inactive(anon):", key))
-            info->anon_inactive_kb = value;
-        else if (0 == strcmp("Active(file):", key))
-            info->file_active_kb = value;
-        else if (0 == strcmp("Inactive(file):", key))
-            info->file_inactive_kb = value;
-        else if (0 == strcmp("Unevictable:", key))
-            info->unevictable_kb = value;
-        else if (0 == strcmp("Mlocked:", key))
-            info->mlocked_kb = value;
         else if (0 == strcmp("SwapTotal:", key))
             info->swap_total_kb = value;
         else if (0 == strcmp("SwapFree:", key))
             info->swap_free_kb = value;
-        else if (0 == strcmp("Dirty:", key))
-            info->dirty_kb = value;
-        else if (0 == strcmp("Writeback:", key))
-            info->write_back_kb = value;
-        else if (0 == strcmp("AnonPages:", key))
-            info->anon_pages_kb = value;
-        else if (0 == strcmp("Mapped:", key))
-            info->mapped_kb = value;
-        else if (0 == strcmp("Shmem:", key))
-            info->shmem_kb = value;
-        else if (0 == strcmp("KReclaimable:", key))
-            info->k_reclaimable_kb = value;
-        else if (0 == strcmp("Slab:", key))
-            info->slab_kb = value;
-        else if (0 == strcmp("SReclaimable:", key))
-            info->s_reclaimable_kb = value;
-        else if (0 == strcmp("SUnreclaim:", key))
-            info->s_unreclaim_kb = value;
-        else if (0 == strcmp("KernelStack:", key))
-            info->kernel_stack_kb = value;
-        else if (0 == strcmp("PageTables:", key))
-            info->page_tables = value;
-        else if (0 == strcmp("NFS_Unstable:", key))
-            info->nfs_unstable_kb = value;
-        else if (0 == strcmp("Bounce:", key))
-            info->bounce_kb = value;
-        else if (0 == strcmp("WritebackTmp:", key))
-            info->write_back_tmp_kb = value;
-        else if (0 == strcmp("CommitLimit:", key))
-            info->commit_limit_kb = value;
-        else if (0 == strcmp("Committed_AS:", key))
-            info->commited_as_kb = value;
         else if (0 == strcmp("VmallocTotal:", key))
             info->vmalloc_total_kb = value;
         else if (0 == strcmp("VmallocUsed:", key))
             info->vmalloc_used_kb = value;
         else if (0 == strcmp("VmallocChunk:", key))
             info->vmalloc_chunk_kb = value;
-        else if (0 == strcmp("Percpu:", key))
-            info->percpu_kb = value;
-        else if (0 == strcmp("HardwareCorrupted:", key))
-            info->hardware_corrupted_kb = value;
-        else if (0 == strcmp("AnonHugePages:", key))
-            info->anon_huge_pages_kb = value;
-        else if (0 == strcmp("ShmemHugePages:", key))
-            info->shmem_huge_pages_kb = value;
-        else if (0 == strcmp("ShmemPmdMapped:", key))
-            info->shmem_pmd_mapped_kb = value;
-        else if (0 == strcmp("FileHugePages:", key))
-            info->file_huge_pages_kb = value;
-        else if (0 == strcmp("FilePmdMapped:", key))
-            info->file_pmd_mapped_kb = value;
-        else if (0 == strcmp("HugePages_Total:", key))
-            info->huge_pages_total = value;
-        else if (0 == strcmp("HugePages_Free:", key))
-            info->huge_pages_free = value;
-        else if (0 == strcmp("HugePages_Rsvd:", key))
-            info->huge_pages_rsvd = value;
-        else if (0 == strcmp("HugePages_Surp:", key))
-            info->huge_pages_surp = value;
-        else if (0 == strcmp("Hugepagesize:", key))
-            info->huge_page_size_kb = value;
-        else if (0 == strcmp("Hugetlb:", key))
-            info->hugetlb_kb = value;
-        else if (0 == strcmp("DirectMap4k:", key))
-            info->direct_map_4k_kb = value;
-        else if (0 == strcmp("DirectMap2M:", key))
-            info->direct_map_2m_kb = value;
-        else if (0 == strcmp("DirectMap1G:", key))
-            info->direct_map_1g_kb = value;
-        else
-            continue;
+        value = 0;
+        memset(key, 0, OS_MEM_BUFF_MAX_LEN);
+        memset(buff, 0, OS_MEM_BUFF_MAX_LEN);
     }
 
 end:
@@ -149,14 +65,14 @@ end:
     return ret;
 }
 
-int os_mem_get_proc_info(const int32_t pid, os_mem_proc_info_t* info)
+int os_mem_get_proc_info(const int32_t pid, os_mem_proc_info_t * info)
 {
     int ret = 0;
-    FILE* fp = NULL;
+    FILE * fp = NULL;
     uint64_t value = 0;
-    char key[OS_MEM_BUFF_MAX_LEN] = { 0 };
-    char buff[OS_MEM_BUFF_MAX_LEN] = { 0 };
-    char path[OS_MEM_BUFF_MAX_LEN] = { 0 };
+    char key[OS_MEM_BUFF_MAX_LEN] = {};
+    char buff[OS_MEM_BUFF_MAX_LEN] = {};
+    char path[OS_MEM_BUFF_MAX_LEN] = {};
 
     if (NULL == info) {
         ret = OS_PERF_ERR_NULLPTR;
@@ -175,12 +91,7 @@ int os_mem_get_proc_info(const int32_t pid, os_mem_proc_info_t* info)
         goto end;
     }
 
-    while (!feof(fp)) {
-        memset(buff, 0, OS_MEM_BUFF_MAX_LEN);
-        fgets(buff, OS_MEM_BUFF_MAX_LEN, fp);
-
-        value = 0;
-        memset(key, 0, OS_MEM_BUFF_MAX_LEN);
+    while (!feof(fp) && fgets(buff, OS_MEM_BUFF_MAX_LEN, fp)) {
         if (2 != sscanf(buff, "%s %Lu", key, &value))
             continue;
 
@@ -190,8 +101,9 @@ int os_mem_get_proc_info(const int32_t pid, os_mem_proc_info_t* info)
             info->vr_kb = value;
         else if (0 == strcmp("VmRSS:", key))
             info->rss_kb = value;
-        else
-            continue;
+        value = 0;
+        memset(key, 0, OS_MEM_BUFF_MAX_LEN);
+        memset(buff, 0, OS_MEM_BUFF_MAX_LEN);
     }
 
 end:
--- src/memory/memory.h
+++ src/memory/memory.h
@@ -13,51 +13,11 @@ typedef struct _os_mem_sys_info_t {
     uint64_t buffers_kb;
     uint64_t cached_kb;
     uint64_t swap_cached_kb;
-    uint64_t active_kb;
-    uint64_t inactive_kb;
-    uint64_t anon_active_kb;
-    uint64_t anon_inactive_kb;
-    uint64_t file_active_kb;
-    uint64_t file_inactive_kb;
-    uint64_t unevictable_kb;
-    uint64_t mlocked_kb;
     uint64_t swap_total_kb;
     uint64_t swap_free_kb;
-    uint64_t dirty_kb;
-    uint64_t write_back_kb;
-    uint64_t anon_pages_kb;
-    uint64_t mapped_kb;
-    uint64_t shmem_kb;
-    uint64_t k_reclaimable_kb;
-    uint64_t slab_kb;
-    uint64_t s_reclaimable_kb;
-    uint64_t s_unreclaim_kb;
-    uint64_t kernel_stack_kb;
-    uint64_t page_tables;
-    uint64_t nfs_unstable_kb;
-    uint64_t bounce_kb;
-    uint64_t write_back_tmp_kb;
-    uint64_t commit_limit_kb;
-    uint64_t commited_as_kb;
     uint64_t vmalloc_total_kb;
     uint64_t vmalloc_used_kb;
     uint64_t vmalloc_chunk_kb;
-    uint64_t percpu_kb;
-    uint64_t hardware_corrupted_kb;
-    uint64_t anon_huge_pages_kb;
-    uint64_t shmem_huge_pages_kb;
-    uint64_t shmem_pmd_mapped_kb;
-    uint64_t file_huge_pages_kb;
-    uint64_t file_pmd_mapped_kb;
-    uint64_t huge_pages_total;
-    uint64_t huge_pages_free;
-    uint64_t huge_pages_rsvd;
-    uint64_t huge_pages_surp;
-    uint64_t huge_page_size_kb;
-    uint64_t hugetlb_kb;
-    uint64_t direct_map_4k_kb;
-    uint64_t direct_map_2m_kb;
-    uint64_t direct_map_1g_kb;
 } os_mem_sys_info_t;
 
 // 进程内存信息
@@ -73,7 +33,7 @@ typedef struct _os_mem_proc_info_t {
 * @param   info   系统内存信息结构体指针
 * @return  >=0--成功 <0--失败
 */
-int os_mem_get_sys_info(os_mem_sys_info_t* info);
+int os_mem_get_sys_info(os_mem_sys_info_t * info);
 
 /**
 * os_mem_get_proc_info
@@ -82,6 +42,6 @@ int os_mem_get_sys_info(os_mem_sys_info_t* info);
 * @param   info   进程内存信息结构体指针
 * @return  >=0--成功 <0--失败
 */
-int os_mem_get_proc_info(int32_t pid, os_mem_proc_info_t* info);
+int os_mem_get_proc_info(int32_t pid, os_mem_proc_info_t * info);
 
 #endif
